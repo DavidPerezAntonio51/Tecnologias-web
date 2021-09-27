883de71eb98071422fa1494955f8a30c11af5b18
Dragonibax <sabino.snm@gmail.com>
2021-09-26 20:29:08 -0500

    Combo box Gradiente

--- BackEnd_Java/2CV13ID5IDT5/src/main/java/administrador_sevlets/Servlet3.java
+++ BackEnd_Java/2CV13ID5IDT5/src/main/java/administrador_sevlets/Servlet3.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package administrador_sevlets;
 
 import com.escom.ipn.cv13id5idt5.Gradiente_script;
