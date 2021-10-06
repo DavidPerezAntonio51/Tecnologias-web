fe9b91219c5e573a9cae45ca3238db08321330fb
David Perez Antonio <david.perez.antonio51@gmail.com>
2021-10-06 14:17:18 -0500

    aprendiendo xml

---
+++ BackEnd_Java/TW12/src/main/java/administrador/Servlet1.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package administrador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author DEZKS
+ */
+public class Servlet1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String cadena = request.getRealPath("/");
+        String cadena1 = this.getServletContext().getRealPath("/");
+        ServletContext app = this.getServletContext();
+        String cadena3 = app.getRealPath("/");
+        
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servlet1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Real path" + cadena + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+    }
+}
